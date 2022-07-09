98e80d2400c3ee2dcb469a31910b5c3ba5fd3464
PBBond <97637272+PBBond@users.noreply.github.com>
2022-07-09 13:26:02 -0500

    got the navbar to work

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Library from "./components/Library";
 import Schedule from "./components/Schedule";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <>
+      <Navbar />
+      <div className="container">
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<Home />} />
           <Route path="/library" element={<Library />} />
           <Route path="/schedule" element={<Schedule />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </>
   );
 }
 
